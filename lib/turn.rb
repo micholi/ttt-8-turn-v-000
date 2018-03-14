a0622af55f3174ea2fada545e7b26375725556ad
Michelle Olivieri <michelleo72@gmail.com>
2018-03-14 16:28:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,7 @@ end
 
 # code for #valid_move? method
 def valid_move?(board, index)
-  index.between?(0, 8) && !position_taken?(board, index)
+  index.between?(0, 8) && position_taken?(board, index) == false
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
