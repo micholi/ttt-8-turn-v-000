a77a8d2b2d1852ec56bb413dda23c1409f63f6a7
Michelle Olivieri <michelleo72@gmail.com>
2018-03-14 16:40:46 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,6 @@ end
 # code for #position_taken? method
 def position_taken?(board, index)
   if board[index] != " " || board[index] != "" || board[index] != nil
-    false
   # elsif board[index] == "X" || board[index] == "O"
     # true
   end
